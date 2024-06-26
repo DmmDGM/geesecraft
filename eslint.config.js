5665a6a9d46736b47a5e2cfba53d257ef1fa9bbb
dm123321_31md <dm12332131mdgaming@gmail.com>
2024-06-26 17:32:47 -0400

    formatted config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
+// Imports
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import ts from "typescript-eslint";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vite';
+// Imports
+import { enhancedImages } from "@sveltejs/enhanced-img";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
+// Exports
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		enhancedImages()
-	]
+	],
+	server: {
+		host: "localhost",
+		port: 3000,
+		strictPort: true
+	}
 });
