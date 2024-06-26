e9d5c52871c24c5915188b0b24797303d044f97c
iiPython <ben@iipython.dev>
2024-06-26 18:23:39 -0500

    vite sucks

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
 		sveltekit(),
 		enhancedImages()
 	],
+	preview: {
+		port: +process.env.PORT || 3000
+	},
 	server: {
 		host: process.env.HOST || "localhost",
 		port: +process.env.PORT || 3000,
