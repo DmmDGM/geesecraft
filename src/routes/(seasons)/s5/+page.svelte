48e9ac6f35652526c8ff4a9c404be72ff652d334
iiPython <ben@iipython.dev>
2024-07-09 14:09:28 -0500

    update s5

--- src/routes/(seasons)/s5/+page.svelte
+++ src/routes/(seasons)/s5/+page.svelte
@@ -4,7 +4,10 @@
 {:then { gallery, id }} 	
 	<!-- Summary -->
 	<div id="summary">
-		hai
+		Check out DmmD's livestreams of the season here (<a href = "https://www.youtube.com/playlist?list=PLU66lEvHdbrvoFRtwEenmLRvfi3--PCIS">direct link</a>):
+		<div class="summary-margin"></div>
+		<iframe width="540" height="304" src="https://www.youtube-nocookie.com/embed/videoseries?si=F1LfxqYfii-c9Fy2&amp;controls=0&amp;list=PLU66lEvHdbrvoFRtwEenmLRvfi3--PCIS" title="Cat Vibe" frameborder="0" allow="encrypted-media; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		<div class="summary-margin"></div>
 		<Gallery {gallery} {id}/>
 	</div>
 {/await}
@@ -29,5 +32,8 @@
 	// Summary
 	#summary {
 		flex: 1;
+	}
+	.summary-margin {
+		margin: 15px;
 	}
 </style>
